902caceb6b9c344623cf6abfc15d9b510954f8c1
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-01 18:20:25 +0900

    add: edge colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  "sainnhe/edge",
+  lazy = false,
+  config = function ()
+    vim.g["edge_style"] = "aura"
+    vim.g["edge_dim_foreground"] = 1
+    vim.g["edge_dim_inactive_windows"] = 1
+    vim.g["edge_better_performance"] = 1
+    vim.cmd[[ colorscheme edge ]]
+  end
+}
