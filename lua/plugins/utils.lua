23d121a2ab81996634fb224407a3e770da5d3f43
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-01 23:07:50 +0900

    add utils plugin

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,9 @@
+return {
+  { "vim-denops/denops.vim", event = { "VeryLazy" } },
+  {
+    "yuki-yano/denops-lazy.nvim",
+    opts = true
+  },
+  { "tani/vim-artemis" },
+  { "nvim-lua/plenary.nvim", lazy = false }
+}
