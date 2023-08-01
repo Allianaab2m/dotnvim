4afb72b15ce3173dc753de8b743a0f915009d24f
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-01 18:28:05 +0900

    add: nvim-surround, lexima

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  },
+  {
+    "cohama/lexima.vim",
+    event = "InsertEnter",
+  },
+}
