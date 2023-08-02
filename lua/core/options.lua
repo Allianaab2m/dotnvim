b2e37e2b6a2541345e7e8d158e27ac5a4a207789
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-03 00:01:32 +0900

    fix: work at other machine

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -37,22 +37,6 @@ for k, v in pairs(options) do
 	vim.o[k] = v
 end
 
-if vim.fn.has("wsl") then
-	vim.g.clipboard = {
-		name = "win32yank-wsl",
-		copy = {
-			["+"] = "win32yank.exe -i --crlf",
-			["*"] = "win32yank.exe -i --crlf",
-		},
-		paste = {
-			["+"] = "win32yank.exe -o --crlf",
-			["*"] = "win32yank.exe -o --crlf",
-		},
-		cache_enable = 0,
-	}
-end
-
 vim.cmd([[set whichwrap+=<,>,[,],h,l]])
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]])
-vim.cmd([[language en_US.utf8]])
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -152,13 +152,9 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		event = "VeryLazy",
 		dependencies = {
-			{
-				dir = "~/ghq/github.com/Allianaab2m/nvim-material-icon-v3",
-			},
 		},
 		config = function()
 			require("nvim-web-devicons").setup({
-				override = require("nvim-material-icon").get_icons(),
 			})
 		end,
 	},
