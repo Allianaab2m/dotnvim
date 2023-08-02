2158771d63daa538ddecd7a0562b34fcac6dfcaa
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-02 22:30:29 +0900

    add: treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	version = false,
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+	opts = {
+		highlight = { enable = true },
+		indent = { enable = true },
+		ensure_installed = {
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"typescript",
+			"tsx",
+			"vim",
+		},
+	},
+	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			local added = {}
+			opts.ensure_installed = vim.tbl_filter(function(lang)
+				if added[lang] then
+					return false
+				end
+				added[lang] = true
+				return true
+			end, opts.ensure_installed)
+		end
+		require("nvim-treesitter.configs").setup(opts)
+		require("treesitter-context").setup()
+	end,
+}
