28b619c9e8806990e643a1bc6e4c72f4db38eea1
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-08 06:36:22 +0900

    remove hardtime

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -5,13 +5,4 @@ return {
 	},
 	{ "tani/vim-artemis" },
 	{ "nvim-lua/plenary.nvim", lazy = false },
-	{
-		"m4xshen/hardtime.nvim",
-		lazy = false,
-		config = function()
-			require("hardtime").setup({
-				disabled_filetypes = { "qf", "netrw", "lazy", "mason", "oil", "toggleterm" },
-			})
-		end,
-	},
 }
