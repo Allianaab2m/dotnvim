9a6553e4d44126783cb0d40a582bc3ea15d008f9
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-08 06:06:32 +0900

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,23 @@
 return {
-	"sainnhe/edge",
-	lazy = false,
-	config = function()
-		vim.g["edge_style"] = "aura"
-		vim.g["edge_dim_foreground"] = 1
-		vim.g["edge_dim_inactive_windows"] = 1
-		vim.g["edge_better_performance"] = 1
-		vim.cmd([[ colorscheme edge ]])
-	end,
+	{
+		"sainnhe/edge",
+		lazy = false,
+		enabled = false,
+		config = function()
+			vim.g["edge_style"] = "aura"
+			vim.g["edge_dim_foreground"] = 1
+			vim.g["edge_dim_inactive_windows"] = 1
+			vim.g["edge_better_performance"] = 1
+			vim.cmd([[ colorscheme edge ]])
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = true,
+		lazy = false,
+		config = function()
+			vim.cmd([[ colorscheme catppuccin ]])
+		end,
+	},
 }
